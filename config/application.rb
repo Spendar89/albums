b5b1d6175bc911f6cb635b48eb593d8a350c6a16
Jake Sendar <jakesendar@gmail.com>
2013-04-01 00:24:02 -0400

    sdfg

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module Albums
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    # config.autoload_paths += %W(#{config.root}/lib)
-#     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.assets.initialize_on_precompile=false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
